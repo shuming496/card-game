a301be12249605b86321a6e293fe0d03559403c6
xiwei <shuming496@outlook.com>
2019-02-19 23:08:43 +0800

    修改调度设置

--- app.rb
+++ app.rb
@@ -15,7 +15,8 @@ class App < Sinatra::Base
   scheduler = Rufus::Scheduler.new
 
   ENV["TZ"] = "Asia/Shanghai"
-  scheduler.at "00:00:00" do
+  scheduler.cron "0 0 * * *" do
+    p "hello"
     db.execute("UPDATE users SET state=? WHERE state=? OR state=?", 0, 1, 2)
     db.execute("UPDATE cards SET gainer_id=?, box_id=? WHERE gainer_id != ?", 0, 0, 0)
     db.execute("DELETE FROM notifications")
